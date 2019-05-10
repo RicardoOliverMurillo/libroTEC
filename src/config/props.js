a575f7b084d252341a9f6327ba40a5e85e5a9019
anjelicat98 <43051211+anjelicat98@users.noreply.github.com>
2019-05-10 16:47:24 -0600

    Modificacion de interfaxz principal

--- src/config/props.js
+++ src/config/props.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    DB: 'mongodb://localhost:27017/LIBROTEC'
+    DB: 'mongodb+srv://admin:12345@clusterlibrotec1-brveg.mongodb.net/librotecdb?retryWrites=true'
 }
